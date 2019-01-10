12e10cf7c46c550de9d767111a3fbd2210cb9eb6
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2019-01-10 13:32:07 -0200

    Simplify plot file
    - Remove duplicated code (6 functions)

--- partidos-politicos/plot.py
+++ partidos-politicos/plot.py
@@ -9,15 +9,15 @@ def plotAll(df, df2, df3, df4, df5, df6):
     plot_num_affiliates(df)
     plot_gender_abs(df)
     plot_gender_relative_bar_full(df)
-    plot_gender_relative_bar_cut(df)
-    plot_gender_relative_line(df)
-    plot_gender_map(df2)
-    plot_candidates_gender_relative_bar_cut(df3)
-    plot_candidates_gender_relative_line(df3)
-    plot_candidates_gender_map(df4)
-    plot_elected_gender_relative_bar_cut(df5)
-    plot_elected_gender_relative_line(df5)
-    plot_elected_gender_map(df6)
+    plot_gender_relative_bar_cut(df, 'Filiados a Partidos Politicos', '4_gender_relative_bar_cut')
+    plot_gender_relative_line(df, 'Filiados a Partidos Politicos', '5_gender_relative_line')
+    plot_gender_map(df2, 'Filiadas por Estado', '6_gender_map')
+    plot_gender_relative_bar_cut(df3, 'Candidatos a Eleicao', '7_candidates_gender_relative_bar_cut')
+    plot_gender_relative_line(df3, 'Candidatos a Eleicao', '8_candidates_relative_line')
+    plot_gender_map(df4, 'Candidatas por Estado', '9_candidates_gender_map')
+    plot_gender_relative_bar_cut(df5, 'Candidatos Eleitos', '10_elected_gender_relative_bar_cut')
+    plot_gender_relative_line(df5, 'Candidatos Eleitos', '11_elected_relative_line')
+    plot_gender_map(df6, 'Eleitas por Estado', '12_elected_gender_map')
     return
 
 
@@ -92,149 +92,7 @@ def plot_gender_relative_bar_full(df):
     return
 
 
-def plot_gender_relative_bar_cut(df):
-    df2 = analyse.make_proportions_clean(df)
-    df2 = df2.sort_values('female')
-
-    n_groups = len(df2)
-    fig, ax = plt.subplots(figsize=(10, 15))
-    index = np.arange(n_groups)
-    bar_width = 0.35
-    opacity = 0.7
-    rects1 = plt.barh(index, df2['female'], bar_width, alpha=opacity, color='r', label='Mulheres')
-    rects2 = plt.barh(index, df2['male'], bar_width, left=df2['female'], alpha=opacity, color='b', label='Homens')
-    rects3 = plt.axvline(x=0.5, color='k', label='Equidade de mulheres e homens', linestyle='--', linewidth=4.0)
-    rects4 = plt.axvline(x=df2['female'].mean(), color='g', label='Proporcao Media de Mulheres', linewidth=2.0)
-    plt.xlabel('Proporcao')
-    plt.ylabel('Partidos')
-    plt.title('Proporcao de genero em Filiados a Partidos Politicos')
-    plt.yticks(index, df2.index)
-    plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-    plt.legend()
-    plt.tight_layout()
-    for spine in plt.gca().spines.values():
-        spine.set_visible(False)
-
-    plt.savefig('test_figure/4_gender_relative_bar_cut.svg', dpi=1000)
-    plt.close(plt.figure())
-    return
-
-
-def plot_gender_relative_line(df):
-    df2 = analyse.make_proportions_clean(df)
-    df2 = df2.sort_values('female')
-
-    n_groups = len(df2)
-    fig, ax = plt.subplots(figsize=(10, 15))
-    index = np.arange(n_groups)
-    opacity = 0.7
-    rects1 = plt.scatter(df2['female'], index, alpha=opacity, color='r', s=np.full(len(df2),200))
-    rects2 = plt.axvline(x=0.5, color='k', label='Equidade de mulheres e homens', linestyle='--', linewidth=4.0)
-    rects3 = plt.axvline(x=df2['female'].mean(), color='g', label='Proporcao Media de Mulheres', linewidth=2.0)
-    plt.xlabel('Proporcao')
-    plt.ylabel('Partidos')
-    plt.title('Proporcao de genero em Filiados a Partidos Politicos')
-    plt.yticks(index, df2.index)
-    plt.xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-    plt.legend()
-    plt.tight_layout()
-    for spine in plt.gca().spines.values():
-        spine.set_visible(False)
-
-    plt.savefig('test_figure/5_gender_relative_line.svg', dpi=1000)
-    plt.close(plt.figure())
-    return
-
-def plot_gender_map(df):
-    df = analyse.make_proportions_clean(df)
-    gdf = gpd.read_file('data/br-states/estados.shp')
-    gdf = gdf.merge(df, left_on='sigla', right_index=True, how='inner')
-    vmin, vmax = 0.0, 0.6
-
-    gdf.plot(column='female', cmap = 'gist_rainbow', legend=True, edgecolor = 'black', vmin=vmin, vmax=vmax, figsize=(15, 9))
-    plt.xticks([])
-    plt.yticks([])
-    for spine in plt.gca().spines.values():
-        spine.set_visible(False)
-    plt.title('Proporcao de Mulheres Filiadas por Estado', fontsize=20)
-    plt.tight_layout()
-    plt.savefig('test_figure/6_gender_map.svg', dpi=1000)
-    plt.close(plt.figure())
-    return
-
-def plot_candidates_gender_relative_bar_cut(df):
-    df2 = analyse.make_proportions_clean(df)
-    df2 = df2.sort_values('female')
-
-    n_groups = len(df2)
-    fig, ax = plt.subplots(figsize=(10, 15))
-    index = np.arange(n_groups)
-    bar_width = 0.35
-    opacity = 0.7
-    rects1 = plt.barh(index, df2['female'], bar_width, alpha=opacity, color='r', label='Mulheres')
-    rects2 = plt.barh(index, df2['male'], bar_width, left=df2['female'], alpha=opacity, color='b', label='Homens')
-    rects3 = plt.axvline(x=0.5, color='k', label='Equidade de mulheres e homens', linestyle='--', linewidth=4.0)
-    rects4 = plt.axvline(x=df2['female'].mean(), color='g', label='Proporcao Media de Mulheres', linewidth=2.0)
-    plt.xlabel('Proporcao')
-    plt.ylabel('Partidos')
-    plt.title('Proporcao de genero dos Candidatos a Eleicao')
-    plt.yticks(index, df2.index)
-    plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-    plt.legend()
-    plt.tight_layout()
-    for spine in plt.gca().spines.values():
-        spine.set_visible(False)
-
-    plt.savefig('test_figure/7_candidates_gender_relative_bar_cut.svg', dpi=1000)
-    plt.close(plt.figure())
-    return
-
-
-def plot_candidates_gender_relative_line(df):
-    df2 = analyse.make_proportions_clean(df)
-    df2 = df2.sort_values('female')
-
-    n_groups = len(df2)
-    fig, ax = plt.subplots(figsize=(10, 15))
-    index = np.arange(n_groups)
-    opacity = 0.7
-    rects1 = plt.scatter(df2['female'], index, alpha=opacity, color='r', s=np.full(len(df2),200))
-    rects2 = plt.axvline(x=0.5, color='k', label='Equidade de mulheres e homens', linestyle='--', linewidth=4.0)
-    rects3 = plt.axvline(x=df2['female'].mean(), color='g', label='Proporcao Media de Mulheres', linewidth=2.0)
-    plt.xlabel('Proporcao')
-    plt.ylabel('Partidos')
-    plt.title('Proporcao de genero dos Candidatos a Eleicao')
-    plt.yticks(index, df2.index)
-    plt.xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-    plt.legend()
-    plt.tight_layout()
-    for spine in plt.gca().spines.values():
-        spine.set_visible(False)
-
-    plt.savefig('test_figure/8_candidates_relative_line.svg', dpi=1000)
-    plt.close(plt.figure())
-    return
-
-
-def plot_candidates_gender_map(df):
-    df = analyse.make_proportions_clean(df)
-    gdf = gpd.read_file('data/br-states/estados.shp')
-    gdf = gdf.merge(df, left_on='sigla', right_index=True, how='inner')
-    vmin, vmax = 0.0, 0.6
-
-    gdf.plot(column='female', cmap = 'gist_rainbow', legend=True, edgecolor = 'black', vmin=vmin, vmax=vmax, figsize=(15, 9))
-    plt.xticks([])
-    plt.yticks([])
-    for spine in plt.gca().spines.values():
-        spine.set_visible(False)
-    plt.title('Proporcao de Mulheres Candidatas por Estado', fontsize=20)
-    plt.tight_layout()
-    plt.savefig('test_figure/9_candidates_gender_map.svg', dpi=1000)
-    plt.close(plt.figure())
-    return
-
-
-def plot_elected_gender_relative_bar_cut(df):
+def plot_gender_relative_bar_cut(df, str1, str2):
     df2 = analyse.make_proportions_clean(df)
     df2 = df2.sort_values('female')
     df2.dropna(subset=['female'], inplace=True)
@@ -250,7 +108,7 @@ def plot_elected_gender_relative_bar_cut(df):
     rects4 = plt.axvline(x=df2['female'].mean(), color='g', label='Proporcao Media de Mulheres', linewidth=2.0)
     plt.xlabel('Proporcao')
     plt.ylabel('Partidos')
-    plt.title('Proporcao de genero dos Candidatos Eleitos')
+    plt.title('Proporcao de Genero em ' + str1)
     plt.yticks(index, df2.index)
     plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
     plt.legend()
@@ -258,12 +116,12 @@ def plot_elected_gender_relative_bar_cut(df):
     for spine in plt.gca().spines.values():
         spine.set_visible(False)
 
-    plt.savefig('test_figure/10_elected_gender_relative_bar_cut.svg', dpi=1000)
+    plt.savefig('test_figure/'+str2+'.svg', dpi=1000)
     plt.close(plt.figure())
     return
 
 
-def plot_elected_gender_relative_line(df):
+def plot_gender_relative_line(df, str1, str2):
     df2 = analyse.make_proportions_clean(df)
     df2 = df2.sort_values('female')
     df2.dropna(subset=['female'], inplace=True)
@@ -277,7 +135,7 @@ def plot_elected_gender_relative_line(df):
     rects3 = plt.axvline(x=df2['female'].mean(), color='g', label='Proporcao Media de Mulheres', linewidth=2.0)
     plt.xlabel('Proporcao')
     plt.ylabel('Partidos')
-    plt.title('Proporcao de genero dos Candidatos Eleitos')
+    plt.title('Proporcao de genero em ' + str1)
     plt.yticks(index, df2.index)
     plt.xticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
     plt.legend()
@@ -285,12 +143,11 @@ def plot_elected_gender_relative_line(df):
     for spine in plt.gca().spines.values():
         spine.set_visible(False)
 
-    plt.savefig('test_figure/11_elected_relative_line.svg', dpi=1000)
+    plt.savefig('test_figure/'+str2+'.svg', dpi=1000)
     plt.close(plt.figure())
     return
 
-
-def plot_elected_gender_map(df):
+def plot_gender_map(df, str1, str2):
     df = analyse.make_proportions_clean(df)
     gdf = gpd.read_file('data/br-states/estados.shp')
     gdf = gdf.merge(df, left_on='sigla', right_index=True, how='inner')
@@ -301,8 +158,8 @@ def plot_elected_gender_map(df):
     plt.yticks([])
     for spine in plt.gca().spines.values():
         spine.set_visible(False)
-    plt.title('Proporcao de Mulheres Eleitas por Estado', fontsize=20)
+    plt.title('Proporcao de Mulheres '+str1, fontsize=20)
     plt.tight_layout()
-    plt.savefig('test_figure/12_elected_gender_map.svg', dpi=1000)
+    plt.savefig('test_figure/'+str2+'.svg', dpi=1000)
     plt.close(plt.figure())
     return
